3e417f6aaeeda387a37e8c305306e9d74ae2246a
Muhammad Umer Bhutta <44334721+MuhammadUmerBhutta@users.noreply.github.com>
2022-02-15 14:44:55 +0500

    Add files via upload

---
+++ scraper_twitter.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 14:39:18 2022
+
+@author: bhutta
+"""
+
+import csv
+import emoji
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common import exceptions
+
+
+def create_webdriver_instance():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    return driver
+
+def login_to_twitter(username, password, driver):
+    url = 'https://twitter.com/login'
+    try:
+        driver.get(url)
+        sleep(10)
+        xpath_username = '//input[@name ="text"]'
+        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.XPATH, xpath_username)))
+        uid_input = driver.find_element_by_xpath(xpath_username)
+        uid_input.send_keys(username)
+        uid_input.send_keys(Keys.RETURN)
+    except exceptions.TimeoutException:
+        print("Timeout while waiting for Login screen")
+        return False
+
+    sleep(10)
+    pwd_input = driver.find_element_by_xpath('//input[@name ="password"]')
+    pwd_input.send_keys(password)
+    try:
+        pwd_input.send_keys(Keys.RETURN)
+        url = "https://twitter.com/home"
+        WebDriverWait(driver, 10).until(expected_conditions.url_to_be(url))
+    except exceptions.TimeoutException:
+        print("Timeout while waiting for home screen")
+    return True
+
+
+def find_search_input_and_enter_criteria(search_term, driver):
+    xpath_search = '//input[@aria-label="Search query"]'
+    search_input = driver.find_element_by_xpath(xpath_search)
+    search_input.send_keys(search_term)
+    search_input.send_keys(Keys.RETURN)
+    return True
+
+def scroll_down_page(driver, last_position, num_seconds_to_load=3, scroll_attempt=0, max_attempts=3):
+    """The function will try to scroll down the page and will check the current
+    and last positions as an indicator. If the current and last positions are the same after `max_attempts`
+    the assumption is that the end of the scroll region has been reached and the `end_of_scroll_region`
+    flag will be returned as `True`"""
+    end_of_scroll_region = False
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    sleep(num_seconds_to_load)
+    curr_position = driver.execute_script("return window.pageYOffset;")
+    if curr_position == last_position:
+        if scroll_attempt < max_attempts:
+            end_of_scroll_region = True
+        else:
+            scroll_down_page(last_position, curr_position, scroll_attempt + 1)
+    last_position = curr_position
+    return last_position, end_of_scroll_region
+
+def save_tweet_data_to_csv(records, filepath, mode='a+'):
+    header = ['User', 'Handle', 'PostDate', 'TweetText']
+    with open(filepath, mode=mode, newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        if mode == 'w':
+            writer.writerow(header)
+        if records:
+            writer.writerow(records)
+
+def generate_tweet_id(tweet):
+    return ''.join(tweet)
+
+def collect_all_tweets_from_current_view(driver, lookback_limit=5):
+    """The page is continously loaded, so as you scroll down the number of tweets returned by this function will
+     continue to grow. To limit the risk of 're-processing' the same tweet over and over again, you can set the
+     `lookback_limit` to only process the last `x` number of tweets extracted from the page in each iteration.
+     You may need to play around with this number to get something that works for you. I've set the default
+     based on my computer settings and internet speed, etc..."""
+    page_cards = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')
+    if len(page_cards) <= lookback_limit:
+        return page_cards
+    else:
+        return page_cards[-lookback_limit:]
+
+def change_page_sort(tab_name, driver):
+    """Options for this program are `Latest` and `Top`"""
+    tab = driver.find_element_by_link_text(tab_name)
+    tab.click()
+    xpath_tab_state = f'//a[contains(text(),\"{tab_name}\") and @aria-selected=\"true\"]'
+
+def extract_data_from_current_tweet_card(card):
+    
+    try:
+        user = card.find_element_by_xpath('.//span').text
+    except exceptions.NoSuchElementException:
+        user = ""
+    except exceptions.StaleElementReferenceException:
+        return
+    try:
+        handle = card.find_element_by_xpath('.//span[contains(text(), "@")]').text
+    except exceptions.NoSuchElementException:
+        handle = ""
+    try:
+        """
+        If there is no post date here, there it is usually sponsored content, or some
+        other form of content where post dates do not apply. You can set a default value
+        for the postdate on Exception if you which to keep this record. By default I am
+        excluding these.
+        """
+        postdate = card.find_element_by_xpath('.//time').get_attribute('datetime')
+    except exceptions.NoSuchElementException:
+        return
+    try:
+        _comment = card.find_element_by_xpath('.//div[2]/div[2]/div[1]').text
+    except exceptions.NoSuchElementException:
+        _comment = ""
+    try:
+        messages = card.find_element_by_xpath('.//div[2]/div[2]/div[2]')
+    except exceptions.NoSuchElementException:
+        messages = ""
+    tweet = messages.text      
+    emojis = card.find_elements_by_class_name("r-4qtqp9")
+    for emojii in emojis:
+        a = emojii.get_attribute('alt')
+        if a != None:
+            a = a.lower()
+            if (' ' in a):
+               a = a.replace(' ', '_')
+            a = ":" + a + ":"
+            tweet = tweet  + emoji.emojize(a) + "---"
+               
+    tweet_text = tweet
+    
+
+    tweet = (user, handle, postdate, tweet_text)
+    return tweet
+
+
+def main(username, password, search_term, filepath, page_sort='Top'):
+    save_tweet_data_to_csv(None, filepath, 'w')  # create file for saving records
+    last_position = None
+    end_of_scroll_region = False
+    unique_tweets = set()
+
+    driver = create_webdriver_instance()
+    logged_in = login_to_twitter(username, password, driver)
+    if not logged_in:
+        return
+    sleep(5)
+    search_found = find_search_input_and_enter_criteria(search_term, driver)
+    if not search_found:
+        return
+    sleep(10)
+    change_page_sort(page_sort, driver)
+
+    while not end_of_scroll_region:
+        cards = collect_all_tweets_from_current_view(driver)
+        for card in cards:
+            try:
+                tweet = extract_data_from_current_tweet_card(card)
+            except exceptions.StaleElementReferenceException:
+                continue
+            if not tweet:
+                continue
+            tweet_id = generate_tweet_id(tweet)
+            if tweet_id not in unique_tweets:
+                unique_tweets.add(tweet_id)
+                save_tweet_data_to_csv(tweet, filepath)
+        last_position, end_of_scroll_region = scroll_down_page(driver, last_position)
+    driver.quit()
+
+
+if __name__ == '__main__':
+    usr = "eftoffe@gmail.com"
+    pwd = "Developer1971"
+    path = 'Sample_tweets_using_webscraper1.csv'
+    term = '🍩🍌'
+    page = 'Top'
+
+    main(usr, pwd, term, path, page)
